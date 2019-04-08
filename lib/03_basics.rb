2f5102aa24f0e5e31b9cba5153964ce8f957dd44
SedTheCursed <sed.robert@gmail.com>
2019-04-08 12:32:07 +0200

    03_basics.rb done

---
+++ lib/03_basics.rb
@@ -0,0 +1,28 @@
+def who_is_bigger(n1, n2, n3)
+    test = {"a" => n1, "b" => n2, "c" => n3}
+    bigger =  "a"
+    biggerValue = n1
+
+    test.each do |k, v|
+        if v == nil
+            return "nil detected"
+        elsif v > biggerValue
+            biggerValue = v
+            bigger = k
+        end
+    end
+
+    bigger + " is bigger"
+end
+
+def reverse_upcase_noLTA(string)
+    string.reverse.upcase.tr("ALT", "")
+end
+
+def array_42(array)
+    array.include?(42)
+end
+
+def magic_array(a)
+    a.flatten.sort.map!{|n| n*2}.select{ |n| n%3!=0}.uniqs
+end
